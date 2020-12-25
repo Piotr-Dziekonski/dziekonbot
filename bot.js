460cf476b81201c79517e22bc5ac2a2541419dd9
@Piotr-Dziekonski <19938986+Piotr-Dziekonski@users.noreply.github.com>
2020-12-25 19:38:22 +0100

    [adding][Travelling Merchant's Shop]
    - added the Travelling Merchant's Shop to bot's commmands under the !tms or !merchant command
    - fixed !vos - it wasn't working due to the fact that the api url had been changed and the old one was not longer correct

--- bot.js
+++ bot.js
@@ -29,6 +29,10 @@ client.on('message', async msg => {
 
     answer = await spooder()
     msg.reply(answer);
+  } else if (['!tms', '!merchant'].includes(msg.content)) {
+
+    answer = await tms()
+    msg.reply(answer);
   }
 
 });
@@ -146,12 +150,11 @@ const vis = async () => {
  */
 const voice = async () => {
   
-  return await fetch('https://chisel.weirdgloop.org/api/runescape/vos')
+  return await fetch('https://api.weirdgloop.org/runescape/vos/history')
     .then(response => response.json())
     .then(data => {
       
-      const voices = data.districts
-      console.log(voices);
+      const voices = [data.data[0].district1, data.data[0].district2]
       voices.forEach((voice, index) => {
         switch (voice) {
           case "Hefin":
@@ -206,6 +209,66 @@ const voice = async () => {
           },
           "thumbnail": {
             "url": "https://vignette.wikia.nocookie.net/runescape2/images/a/a3/Seren_symbol.png/revision/latest?cb=20130811072849"
+          }
+        }
+      }
+
+      return embed
+    });
+
+}
+
+/*
+ *
+ * TRAVELLING MERCHANT'S SHOP
+ *
+ */
+const tms = async () => {
+  return await fetch('https://runescape.wiki/w/Template:Travelling_Merchant')
+    .then(response => response.text())
+    .then(data => {
+      const dom = new JSDOM(data);
+      const table = dom.window.document.querySelector("table.wikitable")
+      const trs = table.querySelectorAll("tr")
+
+      const stock = []
+
+      trs.forEach((elem, index) => {
+        if(index !== 0){
+          const tds = elem.querySelectorAll("td")
+          stock.push([tds[1].textContent, tds[2].textContent]) // [itemName, itemValue] 
+        }
+      });
+
+      console.log(stock)
+
+      const embed = {
+        embed: {
+          "title": "Travelling Merchant's Shop",
+          "color": 370943,
+          "fields": [{
+              "name": stock[0][0],
+              "value": stock[0][1] + " coins",
+            },
+            {
+              "name": stock[1][0],
+              "value": stock[1][1] + " coins",
+            },
+            {
+              "name": stock[2][0],
+              "value": stock[2][1] + " coins",
+            },
+            {
+              "name": stock[3][0],
+              "value": stock[3][1] + " coins",
+            }
+          ],
+          "author": {
+            "name": "Dziekonbot",
+            "icon_url": "https://media.discordapp.net/attachments/428186648199561218/771483063501979648/unknown.png"
+          },
+          "thumbnail": {
+            "url": "https://static.wikia.nocookie.net/runescape2/images/e/e5/Travelling_merchant.png/revision/latest/scale-to-width-down/830?cb=20180312233003"
           }
         }
       }
